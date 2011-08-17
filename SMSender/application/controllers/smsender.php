7ab0def15ad8e470815f09a2fb1b4a61e8def4f9
Mariano Belladonna <mlladon.querox@gmail.com>
2011-08-17 16:07:47 -0300

    Agregando variables para almacenar urls de gateway

--- SMSender/application/controllers/smsender.php
+++ SMSender/application/controllers/smsender.php
@@ -3,6 +3,20 @@
 class smsender extends MY_Controller {
 
     var $template = 'base';
+    var $campaign_id = 4;
+    var $keyword = 'A1';
+    var $gateway_url = '';
+    var $send_pin_url = '';
+    var $check_pin_url = '';
+
+   function __construct() {
+        parent::__construct();
+        if (SITE_URL == 'local'){
+            $this->gateway_url = 'http://localhost/Gateway-Pupolis/index.php/';
+        }
+        $this->send_pin_url = "{$this->gateway_url}gate/sendPin/AmBnZRF2QWf/{$this->keyword}/{$this->campaign_id}/";
+        $this->check_pin_url = "{$this->gateway_url}gate/checkPin/AmBnZRF2QWf/{$this->keyword}/{$this->campaign_id}/";
+    }
 
 	public function index()
 	{
